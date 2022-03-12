15ba00f86f350381b9dbbf5706d7b74ce811e0a2
Vladimir Artemchenko <voartemchenko@gmail.com>
2022-03-12 17:28:14 +0600

    first hot fix

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,20 @@ let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
 let submitButton = document.querySelector('.popup__save-button');
 let popup = document.querySelector('.popup');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__name_username');
+let jobInput = document.querySelector('.popup__name_userjob');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let form = document.querySelector('.popup__container');
 
 function editButtonHandler() {
-    popup.classList.add("popup__type_opened");
+    popup.classList.add("popup_opened");
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
 function closeButtonHandler() {
-    popup.classList.remove("popup__type_opened");
+    popup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
@@ -27,5 +27,4 @@ function formSubmitHandler(evt) {
 
 closeButton.addEventListener("click", closeButtonHandler);
 editButton.addEventListener("click", editButtonHandler);
-submitButton.addEventListener("click", formSubmitHandler);
 form.addEventListener("submit", formSubmitHandler);
